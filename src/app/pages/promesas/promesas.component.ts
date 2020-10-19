386771356af142957163eed9d84e68f4560c21d5
gvidal <gabrielwv1095@gmail.com>
2020-10-19 16:58:52 -0500

    rxjs promises y observables s8

---
+++ src/app/pages/promesas/promesas.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-promesas',
+  templateUrl: './promesas.component.html',
+  styles: [
+  ]
+})
+export class PromesasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    this.getUsuarios().then( usuarios => {
+      console.log(usuarios);
+    });
+     
+    // const promesa = new Promise( (resolve, reject) => {
+
+    //   if(false){
+    //     resolve('Hola Mundo');
+    //   }else{
+    //     reject('Algo Salio mal');
+    //   }
+
+    // });
+
+    // promesa.then( (mensaje) => {
+    //   console.log(mensaje);
+    // }).catch( error => {
+    //   console.log(error);
+    // } );
+
+    // console.log('fin del init');
+
+  }
+
+  getUsuarios(){
+
+    return new Promise( (resolve) => {
+      fetch('https://reqres.in/api/users')
+        .then( resp => resp.json() )
+        .then( body => resolve(body.data));
+
+    } );
+  }
+
+}
